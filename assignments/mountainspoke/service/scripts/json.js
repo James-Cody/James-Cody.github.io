3aec1262c88226663c0be0dbceffdf2ff799279c
James-Cody <codytylorjames@gmail.com>
2018-04-12 19:10:43 -0700

    submit

--- assignments/mountainspoke/service/scripts/json.js
+++ assignments/mountainspoke/service/scripts/json.js
@@ -11,9 +11,7 @@ request.onload = function() {
 }
 
 function populateParts(jsonObj) {
-  var parts = jsonObj['parts'];
-      
-    
+    var parts = jsonObj['parts'];
     var b = [];
     var bw = [];
     var bg = [];
@@ -21,20 +19,19 @@ function populateParts(jsonObj) {
     var bp = [];
     var bs = [];
     
-  for (var i = 0; i < parts.length; i++) {
-    b[i+1] = parts[i].name;
-    bw[i+1] = parts[i].wheel;
-    bg[i+1] = parts[i].gears;
-    bb[i+1] = parts[i].brakes;
-    bp[i+1] = parts[i].pedal;
-    bs[i+1] = parts[i].seat;
-        
-    document.getElementById(b{$i}).innerHTML = b[i+1];
-    document.getElementById(bw{$i}).innerHTML = bw[i+1];
-    document.getElementById(bg{$i}).innerHTML = bg[i+1];
-    document.getElementById(bb{$i}).innerHTML = bb[i+1];
-    document.getElementById(bp{$i}).innerHTML = bp[i+1];
-    document.getElementById(bs{$i}).innerHTML = bs[i+1];
+    for (var i = 0; i < parts.length; i++) {
+        b[i+1] = parts[i].name;
+        bw[i+1] = parts[i].wheel;
+        bg[i+1] = parts[i].gears;
+        bb[i+1] = parts[i].brakes;
+        bp[i+1] = parts[i].pedal;
+        bs[i+1] = parts[i].seat;
+        document.getElementById.toString("b{$i+1}").innerHTML = b[i+1];
+        document.getElementById.toString("bw{$i+1}").innerHTML = bw[i+1];
+        document.getElementById.toString("bg{$i+1}").innerHTML = bg[i+1];
+        document.getElementById.toString("bb{$i+1}").innerHTML = bb[i+1];
+        document.getElementById.toString("bp{$i+1}").innerHTML = bp[i+1];
+        document.getElementById.toString("bs{$i+1}").innerHTML = bs[i+1];
     }
         
 }
