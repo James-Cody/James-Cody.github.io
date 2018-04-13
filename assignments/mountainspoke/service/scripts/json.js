ab7cae61e4dc49d048c7c869d4848da36d523744
James-Cody <codytylorjames@gmail.com>
2018-04-12 20:34:00 -0700

    maybe

--- assignments/mountainspoke/service/scripts/json.js
+++ assignments/mountainspoke/service/scripts/json.js
@@ -1,12 +1,12 @@
-var requestURL = 'https://james-cody.github.io/assignments/mountainspoke/json/prices.json';
+var requestURL = 'https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json';
 var request = new XMLHttpRequest();
 
 request.open('GET', requestURL);
 request.responseType = 'json';
 request.send();
-request.onload = function() {
-  var partArray = request.response;
-  populateParts(partArray);
+request.onload = function(){
+    var partArray = request.response;
+    populateParts(partArray);
 }
 
 function populateParts(jsonObj) {
