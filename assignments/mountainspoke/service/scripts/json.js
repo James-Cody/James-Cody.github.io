54e0555226ec58d07423979c6d1843ceac277000
James-Cody <codytylorjames@gmail.com>
2018-04-12 19:01:18 -0700

    final

--- assignments/mountainspoke/service/scripts/json.js
+++ assignments/mountainspoke/service/scripts/json.js
@@ -20,33 +20,21 @@ function populateParts(jsonObj) {
     var bb = [];
     var bp = [];
     var bs = [];
-    var bplace = 0;
-    var bwplace = 0;
-    var bgplace = 0;
-    var bbplace = 0;
-    var bpplace = 0;
-    var bsplace = 0;
     
   for (var i = 0; i < parts.length; i++) {
     b[i+1] = parts[i].name;
-    bplace = "b" + (i+1);
     bw[i+1] = parts[i].wheel;
-    bwplace = "bw" + (i+1);
     bg[i+1] = parts[i].gears;
-    bgplace = "bg" + (i+1);
     bb[i+1] = parts[i].brakes;
-    bbplace = "bb" + (i+1);
     bp[i+1] = parts[i].pedal;
-    bpplace = "bp" + (i+1);
     bs[i+1] = parts[i].seat;
-    bsplace = "bs" + (i+1);
         
-    document.getElementById(bplace).innerHTML = b[i+1];
-    document.getElementById(bwplace).innerHTML = bw[i+1];
-    document.getElementById(bgplace).innerHTML = bg[i+1];
-    document.getElementById(bbplace).innerHTML = bb[i+1];
-    document.getElementById(bpplace).innerHTML = bp[i+1];
-    document.getElementById(bsplace).innerHTML = bs[i+1];
+    document.getElementById(b{$i}).innerHTML = b[i+1];
+    document.getElementById(bw{$i}).innerHTML = bw[i+1];
+    document.getElementById(bg{$i}).innerHTML = bg[i+1];
+    document.getElementById(bb{$i}).innerHTML = bb[i+1];
+    document.getElementById(bp{$i}).innerHTML = bp[i+1];
+    document.getElementById(bs{$i}).innerHTML = bs[i+1];
     }
         
 }
